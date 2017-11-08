7c6b66358a04ba9a9fb65dd46133a837fee6c142
Andre Rodrigues <andre.silva96@live.com>
2017-11-07 22:38:10 -0200

    Excluindo Post

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PostService} from "../services/post.service";
+import {Post} from "../model/Post";
 
 @Component({
   selector: 'app-post-list',
@@ -7,12 +8,19 @@ import {PostService} from "../services/post.service";
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-
-  posts: any = [];
-  constructor(private post: PostService) { }
-
+  posts: Array<Post> = [];
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
-    this.post.selectPost().subscribe(data => this.posts = data);
+    this.postService.selectPost().subscribe(data => this.posts = data);
+  }
+
+  remove(id: number, index: number) {
+    if (confirm('Deseja excluir esse post?')) {
+      this.postService.remove(id).subscribe(() => {
+          // remove do array
+          this.posts.splice(index, 1);
+      });
+    }
   }
 }
